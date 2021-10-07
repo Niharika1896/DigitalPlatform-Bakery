f3729c8ead9c3571870d3e3ec7528648753d7940
Niharika Sinha <78908196+Niharika1896@users.noreply.github.com>
2021-10-07 12:14:12 -0400

    commit hom.php

---
+++ htdocs/sugarsins/home.php
@@ -0,0 +1,49 @@
+<html>
+	<head>
+		<title> SugarSins | Home</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <style>
+      body {
+        background-color: #ECA8F6;
+      }
+    </style>
+  </head>
+	<body>
+		<nav class="navbar navbar-expand-lg sticky-top navbar-dark" style="background-color: #43024C;">
+			<a class="navbar-brand" href="home.php">SugarSins Admin</a>
+			<div class="navbar-nav">
+				<div class="nav-item dropdown">
+					<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown"><b><font color="white">Order</font></b></a>
+					<div class="dropdown-menu">
+						<a href="neworder.php" class="dropdown-item">New Order</a>
+						<a href="updateorder.php" class="dropdown-item">Update Order</a>
+					</div>
+				</div>
+				</div>
+				<div>
+					<ul class="navbar-nav">
+						<li class="nav-item">
+							<a class="nav-link" href="new_user.php"><b><font color="white">Customer</font></b></a>
+						</li>
+					</ul>
+				</div>
+				<div class="navbar-nav">
+					<div class="nav-item dropdown">
+						<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown"><b><font color="white">Menu</font></b></a>
+						<div class="dropdown-menu">
+							<a href="newmenuitem.php" class="dropdown-item">Add New Items</a>
+							<a href="viewmenu.php" class="dropdown-item">View Menu</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	</body>
+</html>
